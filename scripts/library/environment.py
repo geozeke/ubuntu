087e5e79d93c27b13afef7b53a346a104fd3bf45
Peter Nardi <pete@nardi.com>
2023-06-22 08:58:36 -0400

    Code linting.

--- scripts/library/environment.py
+++ scripts/library/environment.py
@@ -14,14 +14,13 @@ MINOR = 10
 GREEN = "\033[0;32;49m"
 RED = "\033[0;31;49m"
 COLOR_END = "\x1b[0m"
-
 PASS = f"{GREEN}\u2714{COLOR_END}"
 FAIL = f"{RED}\u2718{COLOR_END}"
 DEBUG = False
 
 # Paths in the repo for installation files. Note: Ubuntu is resolved in
 # relation to this file (classes.py) to facilitate debugging. The repo
-# should still be cloned in ~ per the setup instructions..
+# should still be cloned in ~ per the setup instructions.
 HOME = Path.home()
 UBUNTU = Path(__file__).resolve().parents[2]
 OHMYZSH = HOME / ".oh-my-zsh"
